d98374319ffe4d43df48cd34a9fff63b191b149a
bambache@gmail.com <ebambach@users.noreply.github.com>
2017-09-14 16:24:38 -0400

    initial commit of automated testing exercise

---
+++ AutomatedCodeTesting/Fraction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomatedCodeTesting
+{
+    public class Fraction
+    {
+		public int Numerator { get; set; }
+
+		private int _denominator;
+		public int Denominator {
+			get {
+				return _denominator;
+			}
+			set {
+				if (value == 0) {
+					throw new DivideByZeroException("You have broken the universe, press ALT+F4 to restart reality.");
+				}
+				_denominator = value;
+			}
+		}
+
+		public double ToDecimal() {
+			return (double) Numerator/ (double) Denominator;
+		}
+    }
+}
---
+++ TestAutomatedCodeTesting/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutomatedCodeTesting;
+using System.Diagnostics;
+
+namespace TestAutomatedCodeTesting {
+	class Program {
+		void Test(int num, int den, double expected, string testName) {
+			Fraction fraction = new Fraction();
+			fraction.Numerator = num;
+			fraction.Denominator = den;
+
+			var aDecimal = fraction.ToDecimal();
+			var Passed = (aDecimal == -1);
+			Debug.WriteLine($"{testName} returned {aDecimal}, {Passed}");
+		}
+
+		void Run() {
+			foreach(var num in new int[] { -100, -99, -50, -1, 0, 1, 50, 99, 100 }) {
+				foreach (var den in new int[] { -100, -99, -50, -1, 1, 50, 99, 100 }) {
+					Test(num, den, -1, $"Dividing {num} by {den} equals");
+				}
+			}
+			
+			//Test(-100, 100, -1, "Dividing 100 by -100 should be -1");
+			//Test(99, -99, -1, "Dividing 99 by -99 should be -1");
+			Fraction fraction = new Fraction();
+
+			fraction.Numerator = 0;
+			var Passed = false;
+			try {
+				fraction.Denominator = 0;
+			}
+			catch (DivideByZeroException) {
+				Passed = true;
+			}			
+
+			var aDecimal = fraction.ToDecimal();
+			Passed = (aDecimal == 0);
+			Debug.WriteLine($"{aDecimal}, {Passed}");
+		}
+		static void Main(string[] args) {
+			new Program().Run();
+		}
+	}
+}
